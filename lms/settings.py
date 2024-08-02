cdeeab50e0d809dc904f0ac31189bdb45f3a91e4
Ableducation <training@thinnkware.com>
2024-08-02 12:34:37 +0530

    changes done in settings

--- lms/settings.py
+++ lms/settings.py
@@ -62,11 +62,12 @@ SITE_ID = 1
 SOCIALACCOUNT_LOGIN_ON_GET=True
 CSRF_TRUSTED_ORIGINS = [
     'https://ablskool.com',
-    # 'https://www.ablskool.com',
+    'https://www.ablskool.com',
 ]
 
 SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 
 
